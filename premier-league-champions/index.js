e051599fa4af4ff56d2967bd8b0a72431e4df6d8
Valenza <matheusvvalenza@gmail.com>
2020-08-15 11:50:44 -0300

    added tests and finished challenge

--- premier-league-champions/index.js
+++ premier-league-champions/index.js
@@ -9,7 +9,8 @@ function calculatePerformance(team) {
 function getPositions(t1, t2) {
   if (t1.points > t2.points) return -1;
 
-  if (t1.points === t2.points && t1.goalsDifference > t2.points) return -1;
+  if (t1.points === t2.points && t1.goalsDifference > t2.goalsDifference)
+    return -1;
 
   return 1;
 }
@@ -17,34 +18,5 @@ function getPositions(t1, t2) {
 function champions(teams) {
   return teams.map(calculatePerformance).sort(getPositions).shift().name;
 }
-
-console.log(
-  champions([
-    {
-      name: 'Manchester United',
-      wins: 30,
-      loss: 3,
-      draws: 5,
-      scored: 88,
-      conceded: 20
-    },
-    {
-      name: 'Arsenal',
-      wins: 24,
-      loss: 6,
-      draws: 8,
-      scored: 98,
-      conceded: 29
-    },
-    {
-      name: 'Chelsea',
-      wins: 22,
-      loss: 8,
-      draws: 8,
-      scored: 98,
-      conceded: 29
-    }
-  ])
-);
 
 module.exports = champions;
--- premier-league-champions/test.spec.js
+++ premier-league-champions/test.spec.js
@@ -1,5 +1,173 @@
 const champions = require('.');
 
 test('it passes in platform tests', () => {
-  expect(champions()).toBe(true);
+  expect(
+    champions([
+      {
+        name: 'Manchester United',
+        wins: 30,
+        loss: 3,
+        draws: 5,
+        scored: 88,
+        conceded: 20
+      },
+      {
+        name: 'Arsenal',
+        wins: 24,
+        loss: 6,
+        draws: 8,
+        scored: 98,
+        conceded: 29
+      },
+      {
+        name: 'Chelsea',
+        wins: 22,
+        loss: 8,
+        draws: 8,
+        scored: 98,
+        conceded: 29
+      }
+    ])
+  ).toBe('Manchester United');
+  expect(
+    champions([
+      {
+        name: 'Manchester City',
+        wins: 30,
+        loss: 8,
+        draws: 0,
+        scored: 67,
+        conceded: 20
+      },
+      {
+        name: 'Liverpool',
+        wins: 34,
+        loss: 2,
+        draws: 2,
+        scored: 118,
+        conceded: 29
+      },
+      {
+        name: 'Leicester City',
+        wins: 22,
+        loss: 8,
+        draws: 8,
+        scored: 98,
+        conceded: 29
+      }
+    ])
+  ).toBe('Liverpool');
+
+  expect(
+    champions([
+      {
+        name: 'Manchester City',
+        wins: 30,
+        loss: 8,
+        draws: 0,
+        scored: 67,
+        conceded: 20
+      },
+      {
+        name: 'NewCastle United',
+        wins: 34,
+        loss: 2,
+        draws: 2,
+        scored: 118,
+        conceded: 36
+      },
+      {
+        name: 'Leicester City',
+        wins: 34,
+        loss: 2,
+        draws: 2,
+        scored: 108,
+        conceded: 21
+      }
+    ])
+  ).toBe('Leicester City');
+  expect(
+    champions([
+      {
+        name: 'Manchester City',
+        wins: 30,
+        loss: 6,
+        draws: 2,
+        scored: 102,
+        conceded: 20
+      },
+      {
+        name: 'Liverpool',
+        wins: 24,
+        loss: 6,
+        draws: 8,
+        scored: 118,
+        conceded: 29
+      },
+      {
+        name: 'Arsenal',
+        wins: 28,
+        loss: 2,
+        draws: 8,
+        scored: 87,
+        conceded: 39
+      }
+    ])
+  ).toBe('Manchester City');
+  expect(
+    champions([
+      {
+        name: 'Manchester City',
+        wins: 30,
+        loss: 6,
+        draws: 2,
+        scored: 102,
+        conceded: 20
+      },
+      {
+        name: 'Liverpool',
+        wins: 24,
+        loss: 6,
+        draws: 8,
+        scored: 118,
+        conceded: 29
+      },
+      {
+        name: 'Arsenal',
+        wins: 30,
+        loss: 0,
+        draws: 8,
+        scored: 87,
+        conceded: 39
+      }
+    ])
+  ).toBe('Arsenal');
+  expect(
+    champions([
+      {
+        name: 'Chelsea',
+        wins: 35,
+        loss: 3,
+        draws: 0,
+        scored: 102,
+        conceded: 20
+      },
+      {
+        name: 'Liverpool',
+        wins: 24,
+        loss: 6,
+        draws: 8,
+        scored: 118,
+        conceded: 29
+      },
+      {
+        name: 'Arsenal',
+        wins: 28,
+        loss: 2,
+        draws: 8,
+        scored: 87,
+        conceded: 39
+      }
+    ])
+  ).toBe('Chelsea');
 });
