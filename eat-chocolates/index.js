c8b605789bd7e3d622470058573c525c3be84b05
Valenza <matheusvvalenza@gmail.com>
2020-09-21 13:55:29 -0300

    added invalid input handler

--- eat-chocolates/index.js
+++ eat-chocolates/index.js
@@ -1,16 +1,22 @@
 const getMoneyFromInput = (moneyInput) => {
-  const pattern = /(\d+)/g;
+  const pattern = /(\-?\d+)/g;
   return +pattern.exec(moneyInput)[0];
 }
 
 function countChocolates(moneyInput, costInput) {
   const money = getMoneyFromInput(moneyInput);
   const cost = getMoneyFromInput(costInput);
+
+  if (money < 0 || cost < 0)
+    return 'Invalid Input'
   
-  console.log(money, cost);
+  const boughtChocolates = money/cost;
+
+  console.log(money, cost, boughtChocolates);
 }
 
 // console.log(countChocolates("4$", "1$")); // 5
-countChocolates("I have 68$", "2$") // 50
+// countChocolates("I have 68$", "2$") // 50
+console.log(countChocolates("I got -68$ from my mom ", "2$")) // Invalid Input
 
 module.exports = countChocolates;
