7b33a494566f42e104be700982b3d4d6909def7e
Valenza <matheusvvalenza@gmail.com>
2020-09-21 13:49:47 -0300

    added entry parse

--- eat-chocolates/index.js
+++ eat-chocolates/index.js
@@ -1,7 +1,15 @@
-function countChocolates(money, cost) {
-	
+const getMoneyFromInput = (moneyInput) => {
+  const pattern = /(\d+)/g;
+  return +pattern.exec(moneyInput)[0];
 }
 
-console.log(countChocolates("4$", "1$")); // 5
+function countChocolates(moneyInput, cost) {
+  const money = getMoneyFromInput(moneyInput);
+  
+  console.log(money);
+}
+
+// console.log(countChocolates("4$", "1$")); // 5
+countChocolates("I have 68$", "2$") // 50
 
 module.exports = countChocolates;
